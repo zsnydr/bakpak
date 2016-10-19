654fde2f4e52bde0e2a67335708d1780f3918fd8
Jason <jmaier@berkeley.edu>
2016-10-19 13:34:28 -0700

    Got rid of controllers

--- Controllers/beercontroller.js
+++
@@ -1,6 +0,0 @@
-var BreweryDb = require('brewerydb-node');
-var brewdb = new BreweryDb('6a9295f5652fd5ef1ed7bf0fd6bae84b');
-var express  = require('express');
-var router = express.Router();
-
-router.get('/beerme')
