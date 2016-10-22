87b4e3077e12d77c6f07a86a1bdd194d7c75afd4
Gery Egan <gery.egan@gmail.com>
2016-10-21 20:47:43 -0700

    added basic translation functionality

--- server.js
+++ server.js
@@ -84,6 +84,19 @@ app.post('/events', function(req,res){
 
   })
 })
+app.post('/translate', function(req,res){
+  query.text=req.body.inputText
+  query.country = 'en-it';
+  var queryTranslate = query.translate + keys.yandex + '&text=' + query.text + '&lang=' + query.country;
+
+  request(queryTranslate, function(error, resp, body){
+    if(error) {
+      console.log(error);
+    }
+    console.log(resp.body);
+    res.end(resp.body);    
+  })
+})
 
 options = { //for qpx
   key: keys.google,
