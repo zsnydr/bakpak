b5e8afeaeda719bb11582e67dd276ed483def7fa
Jing Michelle Li <jing.m.li0607@gmail.com>
2016-10-24 12:30:18 -0700

    minor change to fix scope.city

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ var keys = {
 	sqoot: 'yfnxm3',
 	eventful: 'pvzhm2F4B5MwtBbB',
 
-	google: 'AIzaSyA5uw6lUl_UMbbU1Q44sMhHDnzuh01Xs7M',
-	// google: 'AIzaSyC4bdaVcDwKdl-iNcFUkOuwi0_pzptNfZ0',
+	// google: 'AIzaSyA5uw6lUl_UMbbU1Q44sMhHDnzuh01Xs7M',
+	google: 'AIzaSyC4bdaVcDwKdl-iNcFUkOuwi0_pzptNfZ0',
 	yandex: 'trnsl.1.1.20161013T163156Z.2567488658ef9612.1c3f578efcfa73e08cf35738bc170a455c03fcd8'
 }
 
--- public/bakpakexplore.js
+++ public/bakpakexplore.js
@@ -281,7 +281,7 @@ angular.module('bakpak.explore', [])
 		$http({
 		  method: 'POST',
 		  url: '/weather',
-		  data: {city: $scope.selected}
+		  data: {city: $scope.city}
 		})
 		.then(function(data){
 		  $scope.weather = data.data;
@@ -293,7 +293,7 @@ angular.module('bakpak.explore', [])
 		$http({
 		  method: 'POST',
 		  url: '/arts',
-		  data: {city: $scope.selected}
+		  data: {city: $scope.city}
 		})
 		.then(function(data){
 		  $scope.arts = data.data.results;
@@ -305,7 +305,7 @@ angular.module('bakpak.explore', [])
 		$http({
 		  method: 'POST',
 		  url: '/promos',
-		  data: {city: $scope.selected}
+		  data: {city: $scope.city}
 		})
 		.then(function(data){
 		  $scope.promos = data.data.deals;
