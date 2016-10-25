958f47a25180fdd9eaae87fa3587556c9561cab1
Zack Snyder <zackwsnyder@gmail.com>
2016-10-25 14:35:02 -0700

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-var keys = {
-	weather: '5b084e8c99133e1e96cebc07a2d555f7',
-	google:'AIzaSyAFgnVhCqHSnnYndzQjwVZPT5HpsTWeyhQ',
-	sqoot: '2Ivvf8f0iNW6yOAJ7dVr',
-	eventful: 'kLXj2TcXtKGNttjP',
-	yandex: 'trnsl.1.1.20161025T211215Z.3d6e00b2a86cefcc.28a116432a5347c6bbe2cfabde1ad2555cc6defb'
-}
-
-module.exports = keys;
