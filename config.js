a4e8edfe5e57af9cc4d42aa9aa517b3baf005714
Jing Michelle Li <jing.m.li0607@gmail.com>
2016-10-25 10:56:41 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,8 @@
 var keys = {
 	weather: '83f61e001de33d8e3dfae71ec0234172',
-	// google: 'AIzaSyAdT6BVjQ_pKohHEUAbweR3dD0QNHx69G8',
+	// google:,
 	sqoot: 'yfnxm3',
 	eventful: 'pvzhm2F4B5MwtBbB',
-
-	// google: 'AIzaSyA5uw6lUl_UMbbU1Q44sMhHDnzuh01Xs7M',
-	google: 'AIzaSyByS9AcDMH3IL7nDQJxWPsh-aTC4OLe1pE',
 	yandex: 'trnsl.1.1.20161013T163156Z.2567488658ef9612.1c3f578efcfa73e08cf35738bc170a455c03fcd8'
 }
 
