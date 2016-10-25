52e5cb9075009c022d5d73b604325e4e0fafbc2f
Jing Michelle Li <jing.m.li0607@gmail.com>
2016-10-25 10:57:26 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 var keys = {
-	weather: '83f61e001de33d8e3dfae71ec0234172',
-	// google:,
-	sqoot: 'yfnxm3',
-	eventful: 'pvzhm2F4B5MwtBbB',
-	yandex: 'trnsl.1.1.20161013T163156Z.2567488658ef9612.1c3f578efcfa73e08cf35738bc170a455c03fcd8'
+	weather: '',
+	// google:'',
+	sqoot: '',
+	eventful: '',
+	yandex: ''
 }
 
 module.exports = keys;
