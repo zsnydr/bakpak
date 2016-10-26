4255a9a42017f019b40ace8d11e8dca571dc07d8
Conor <oleary.conor17@gmail.com>
2016-10-26 14:57:51 -0700

    [ADD] authentication routing

--- public/services.js
+++ public/services.js
@@ -1,8 +1,9 @@
 angular.module('app-services', [])
-.factory('Auth', ['$http', function($http, username, password){
+.factory('Auth', ['$http', '$window', function($http, $window, username, password){
 
   var signin = function(username, password){
     console.log('correct');
+    console.log('user: ', username, 'pass: ', password)
     return $http({
       method: 'POST',
       url: '/signin',
@@ -10,6 +11,12 @@ angular.module('app-services', [])
         username: username,
         password: password
         }
+    }).then(function(res){
+      if(res.data === 'matched'){
+        $window.location = '/#/explore'
+      } else {
+        $window.location = '/#/signin'
+      }
     })
   }
 
@@ -21,6 +28,12 @@ angular.module('app-services', [])
         username: username,
         password: password
         }
+    }).then(function(res){
+      if(res.data === 'user exists'){
+        $window.location = '/#/signup'
+      } else {
+        $window.location = '/#explore'
+      }
     })
   }
 
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 var routes = require('./router.js');
+var session = require('express-session');
 
 require('dotenv').config();
 
@@ -11,7 +12,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname));
-
+app.use(session({
+  secret: 'secretkey',
+  resave: true,
+  saveUninitialized: true,
+  cookie: {maxAge: 300*300*300}
+}))
 routes.router(app);
 
 app.listen(process.env.PORT || 3000);
