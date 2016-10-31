4cac2b1adc35ced07beab644f1c207c05c8dd6a9
zsnydr <zackwsnyder@gmail.com>
2016-10-31 08:21:54 -0700

    [refactor] code cleanup

--- public/services.js
+++ public/services.js
@@ -1,17 +1,17 @@
 angular.module('app-services', ['app-services'])
+
 .factory('Auth', ['$http', '$window', function($http, $window, username, password){
+
   var isSignedIn;
 
-  var signin = function(username, password){
-    console.log('correct');
-    console.log('user: ', username, 'pass: ', password)
+  var signin = function(username, password) {
     return $http({
       method: 'POST',
       url: '/signin',
       data: {
         username: username,
         password: password
-        }
+      }
     }).then(function(res){
       if(res.data === 'matched'){
         isSignedIn = true;
@@ -21,16 +21,16 @@ angular.module('app-services', ['app-services'])
         return res.data;
       }
     });
-  }
+  };
 
-  var signup = function(username, password){
+  var signup = function(username, password) {
     return $http({
       method: 'POST',
       url: '/signup',
       data: {
         username: username,
         password: password
-        }
+      }
     }).then(function(res){
       if(res.data === 'user already exists' || res.data === 'no user'){
         return res.data;
@@ -42,8 +42,8 @@ angular.module('app-services', ['app-services'])
     })
     .catch(function(err){
       console.error(err)
-    })
-  }
+    });
+  };
 
   var signout = function () {
     return $http({
@@ -51,23 +51,23 @@ angular.module('app-services', ['app-services'])
       url: '/signout',
       data: {}
     }).then(function(data){
-      console.log('SIGNOUT SUCCESS', data);
       isSignedIn = false;
-      $window.location = '/#/signin'
-    })
-  }
+      $window.location = '/#/signin';
+    });
+  };
 
   var signedIn = function(){
     return isSignedIn;
-  }
-return {
-  signin: signin,
-  signup: signup,
-  signout: signout,
-  isSignedIn: signedIn
-}
+  };
 
-}])
+  return {
+    signin: signin,
+    signup: signup,
+    signout: signout,
+    isSignedIn: signedIn
+  }
+
+}]);
 
 .factory('Trips', ['$http', '$location', '$timeout', function($http, $location, $timeout){
 
@@ -84,7 +84,7 @@ return {
       }
       return data;
     });
-  }
+  };
 
   var saveTrip = function(city, tripTitle) {
     return $http({
@@ -93,23 +93,19 @@ return {
       data: {city: city, title: tripTitle}
     })
     .then(function(tripData){
-      console.log('TRIP DATA', tripData)
       tripId = tripData.data.trip_id;
-      console.log('TRIP id in factory', tripId)
-
-
-      $location.path('/explore')
+      $location.path('/explore');
       return tripData;
-    })
-  }
+    });
+  };
 
   var setTripId = function(data) {
     tripId = data;
-  }
+  };
 
   var getTripId = function() {
     return tripId;
-  }
+  };
 
   var getTrips = function() {
     return $http({
@@ -121,8 +117,8 @@ return {
         $window.location = '/#/signin';
       }
       return trips;
-    })
-  }
+    });
+  };
 
   return {
     isLoggedIn: isLoggedIn,
@@ -133,47 +129,24 @@ return {
     setTripId: setTripId
   }
 
-}])
+}]);
 
-.factory('CityService', function () {
+.factory('CityService', function() {
   var city = '';
 
   var setCity = function(data) {
-    if(data === '') {
-      console.log(true);
-    }
-    console.log('IN SET CITY', data)
     city = data;
-  }
+  };
 
   var getCity = function() {
     return city;
-  }
+  };
 
   return {
     setCity: setCity,
     getCity: getCity
   }
-})
-
-  .service('IdService', function () {
-  var id = '';
-
-  var setId = function(data) {
-    console.log('IN SET ID', data)
-    id = data;
-  }
-
-  var getId = function() {
-    return id;
-  }
-
-  return {
-    setId: setId,
-    getId: getId
-  }
-
-})
+});
 
 .factory('Save', ['$http', function ($http) {
 
@@ -184,10 +157,9 @@ return {
       data: object
     })
     .then(function(data) {
-      console.log('savePlace SUCCESS', data)
       return data;
-    })
-  }
+    });
+  };
 
   var saveRestaurant = function (object) {
     $http({
@@ -196,10 +168,9 @@ return {
       data: object
     })
     .then(function(data) {
-       console.log('saveRestaurant SUCCESS', data)
       return data;
-    })
-  }
+    });
+  };
 
   var saveEvent = function (object) {
     $http({
@@ -208,35 +179,31 @@ return {
       data: object
     })
     .then(function(data) {
-      console.log('saveEvent SUCCESS', data)
       return data;
-    })
-  }
+    });
+  };
 
-   var saveHotel = function (object) {
+  var saveHotel = function (object) {
     $http({
       method: 'POST',
       url: '/saveHotel',
       data: object
     })
     .then(function(data) {
-      console.log('saveEvent SUCCESS', data)
       return data;
-    })
-  }
+    });
+  };
 
-   var saveFlight = function (object) {
+  var saveFlight = function (object) {
     $http({
       method: 'POST',
       url: '/saveFlight',
       data: object
     })
     .then(function(data) {
-      console.log('saveEvent SUCCESS', data)
       return data;
-    })
-  }
-
+    });
+  };
 
   return {
     savePlace: savePlace,
@@ -245,11 +212,7 @@ return {
     saveHotel: saveHotel,
     saveFlight: saveFlight
   }
-
-
-
-}])
-
+}]);
 
 .factory('Remove', ['$http', function ($http) {
 
@@ -260,10 +223,9 @@ return {
       data: object
     })
     .then(function(data) {
-      console.log('removePlace SUCCESS', data)
       return data;
-    })
-  }
+    });
+  };
 
   var removeRestaurant = function (object) {
     return $http({
@@ -272,10 +234,9 @@ return {
       data: object
     })
     .then(function (data) {
-       console.log('removeRestaurant SUCCESS', data)
       return data;
-    })
-  }
+    });
+  };
 
   var removeEvent = function (object) {
     return $http({
@@ -284,34 +245,31 @@ return {
       data: object
     })
     .then(function(data) {
-      console.log('removeEvent SUCCESS', data)
       return data;
-    })
-  }
+    });
+  };
 
-   var removeHotel = function (object) {
+  var removeHotel = function (object) {
     return $http({
       method: 'PUT',
       url: '/removeHotel',
       data: object
     })
     .then(function(data) {
-      console.log('removehotel SUCCESS', data)
       return data;
-    })
-  }
+    });
+  };
 
-   var removeFlight = function (object) {
+  var removeFlight = function (object) {
     return $http({
       method: 'PUT',
       url: '/removeFlight',
       data: object
     })
     .then(function(data) {
-      console.log('removeflight SUCCESS', data)
       return data;
-    })
-  }
+    });
+  };
 
   var removeTrip = function (object) {
     return $http({
@@ -320,24 +278,20 @@ return {
       data: object
     })
     .then(function(data) {
-      console.log('removetrip SUCCESS', data)
       return data;
-    })
-  }
+    });
+  };
 
   var removeDestination = function (object) {
-    console.log("REMOVE DEST OBJ", object)
     return $http({
       method: 'PUT',
       url: '/removeDestination',
       data: object
     })
     .then(function(data) {
-      console.log('removedestination SUCCESS', data)
       return data;
-    })
-  }
-
+    });
+  };
 
   return {
     removePlace: removePlace,
@@ -348,7 +302,4 @@ return {
     removeTrip: removeTrip,
     removeDestination: removeDestination
   }
-
-
-
-}])
+}]);
